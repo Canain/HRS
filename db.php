8437b00f48ae04bf131058b93455b88e525818c7
Shuyang Chen <sc1085@gmail.com>
2015-11-30 17:37:41 -0500

    Fixed registration

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
 session_start();
-$db = new PDO('mysql:host=127.0.0.1;dbname=cs4400_Group_30', 'root', 'root');
+$db = new PDO('mysql:host=127.0.0.1;dbname=cs4400_Group_30', 'root', '');
--- registration.php
+++ registration.php
@@ -36,16 +36,10 @@ if (isset($_POST["username"])) {
         <div class="row">
         <form class="col s8" method="post">
           <div class="row">
-            <div class="input-field col s8">
-              <input id="username" name="username" type="text" class="validate">
-              <label for="username">Username</label>
-            </div>
-          </div>
-          <div class="row">
-          <div class="input-field col s8">
-              <input id="username" type="email" class="validate">
-              <label for="username">Username</label>
-            </div>
+              <div class="input-field col s8">
+                  <input id="username" name="username" type="text" class="validate">
+                  <label for="username">Username</label>
+              </div>
             <div class="input-field col s8">
               <input id="password" name="password" type="password" class="validate">
               <label for="disabled">Password</label>
