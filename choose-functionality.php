5d5988733094ae8f75a2bc17b3d680439249e82c
nikhil-kulkarni <nikhil896@gmail.com>
2015-12-01 14:16:00 -0500

    Centered options

--- choose-functionality.php
+++ choose-functionality.php
@@ -2,30 +2,31 @@
 require 'base.php';
 require 'start.php';
 ?>
-<div class="row">
+<div style="display:flex;justify-content: center;align-items: center; position: relative;">
+    <div class="row">
+        <div class="col s12">
+            <!--  SHOWN FOR CUSTOMER  -->
+            <?php if (!$_SESSION["manager"]) : ?>
+                <div class="collection">
+                    <a href="make-reservation.php" class="collection-item">Make a new reservation</a>
+                    <a href="update-reservation.php" class="collection-item">Update your reservation</a>
+                    <a href="cancel-reservation.php" class="collection-item">Cancel reservation</a>
+                    <a href="give-review.php" class="collection-item">Provide feedback</a>
+                    <a href="view-review.php" class="collection-item">View feedback</a>
+                </div>
+            <?php endif; ?>
+        </div>
 
-    <div class="col s6">
-        <!--  SHOWN FOR CUSTOMER  -->
-        <?php if (!$_SESSION["manager"]) : ?>
-            <div class="collection">
-                <a href="make-reservation.php" class="collection-item">Make a new reservation</a>
-                <a href="update-reservation.php" class="collection-item">Update your reservation</a>
-                <a href="cancel-reservation.php" class="collection-item">Cancel reservation</a>
-                <a href="give-review.php" class="collection-item">Provide feedback</a>
-                <a href="view-review.php" class="collection-item">View feedback</a>
-            </div>
-        <?php endif; ?>
-    </div>
-
-    <div class="col s6">
-        <!--  SHOWN FOR MANAGEMENT  -->
-        <?php if ($_SESSION["manager"]) : ?>
-            <div class="collection">
-                <a href="view-reservation-report.php" class="collection-item">View Reservation report</a>
-                <a href="view-popular-category.php" class="collection-item">View popular room category report</a>
-                <a href="view-revenue-report.php" class="collection-item">View revenue report</a>
-            </div>
-        <?php endif; ?>
+        <div class="col s12">
+            <!--  SHOWN FOR MANAGEMENT  -->
+            <?php if ($_SESSION["manager"]) : ?>
+                <div class="collection">
+                    <a href="view-reservation-report.php" class="collection-item">View Reservation report</a>
+                    <a href="view-popular-category.php" class="collection-item">View popular room category report</a>
+                    <a href="view-revenue-report.php" class="collection-item">View revenue report</a>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
 <?php require 'end.php';
