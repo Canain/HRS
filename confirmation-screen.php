15221897f05bcbab51aac78dd68b758ef9bc21cb
othornycroft <othornycroft@gmail.com>
2015-12-01 00:36:38 -0500

    Confirmation screen php

--- confirmation-screen.php
+++ confirmation-screen.php
@@ -3,6 +3,13 @@ require 'base.php';
 require 'start.php';
 ?>
 <h2>Confirmation</h2>
-<h4 id='reservation_id'><!-- variable: reservation_id --> Your Reservation ID: </h4>
+<h4 id='reservation_id'><!-- variable: reservation_id --> Your Reservation ID:
+<?php
+$sql = "SELECT max(reservation_id) FROM reservation WHERE username = :username;";
+$st = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$st->execute(array(':username' => $_SESSION["username"]));
+$reservation = $st->fetch();
+echo $reservation['id'];
+?></h4>
 <h6>Please save this reservation id for all further communication</h6>
 <?php require 'end.php';
