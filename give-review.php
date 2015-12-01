6dac8c6636d0b9013be0c0eb5f217cf766aa8182
othornycroft <othornycroft@gmail.com>
2015-11-30 20:25:19 -0500

    Give review php functionality - might not work

--- give-review.php
+++ give-review.php
@@ -1,3 +1,20 @@
+<?php
+require 'db.php';
+if (isset($_POST["location-dropdown"]) && isset($_POST["rating-dropdown"]) && isset($_POST["comment"])) {
+  $id = round(microtime(true) * 1000);
+  $location = $_POST["location-dropdown"];
+  $rating = $_POST["rating-dropdown"];
+  $comment = $_POST["comment"];
+  $username = $_SESSION["username"];
+  $sql = 'INSERT INTO review VALUE (:id, :location, :rating, :comment, :username)';
+  $st = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+  $st->execute(array(':id' => $id, ':location' => $location, ':rating' => $rating,
+      ':comment' => $comment, ':username' => $username));
+} else {
+  print "Not all of the options are set";
+}
+?>
+
   <!DOCTYPE html>
   <html>
     <head>
@@ -27,25 +44,31 @@
           <li><a href="#!">Miami</a></li>
         </ul>
 
+        <select name="location-dropdown" class="browser-default">
+          <option disabled selected value="">Location</option>
+          <option value="Atlanta">Atlanta</option>
+          <option value="Charlotte">Charlotte</option>
+          <option value="Savannah">Savannah</option>
+          <option value="Orlando">Orlando</option>
+          <option value="Miami">Miami</option>
+        </select>
+
         <p></p>
 
-        <!-- Dropdown Trigger -->
-        <a class='dropdown-button btn' href='#' data-activates='dropdown2'>Rating</a>
-
-        <!-- Dropdown Structure -->
-        <ul id='dropdown2' class='dropdown-content'>
-          <li><a href="#!">Excellent</a></li>
-          <li><a href="#!">Good</a></li>
-          <li><a href="#!">Bad</a></li>
-          <li><a href="#!">Very Bad</a></li>
-          <li><a href="#!">Neutral</a></li>
-        </ul>
+        <select name="rating-dropdown" class="browser-default">
+          <option disabled selected value="">Rating</option>
+          <option value="Excellent">Atlanta</option>
+          <option value="Good">Charlotte</option>
+          <option value="Bad">Savannah</option>
+          <option value="Very Bad">Orlando</option>
+          <option value="Neutral">Miami</option>
+        </select>
 
         <div class="row">
           <form class="col s12">
             <div class="row">
               <div class="input-field col s12">
-                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                <textarea id="textarea1" name="comment" class="materialize-textarea"></textarea>
                 <label for="textarea1">Comment</label>
               </div>
             </div>
