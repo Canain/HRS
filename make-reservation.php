51ee42268a91ed0841bd78b6bac45182cd352b85
Shuyang Chen <sc1085@gmail.com>
2015-12-01 15:12:52 -0500

    Finished basic functionality of make reservation

--- make-reservation.php
+++ make-reservation.php
@@ -1,5 +1,47 @@
 <?php
 require 'base.php';
+function startsWith($haystack, $needle) {
+    $length = strlen($needle);
+    return (substr($haystack, 0, $length) === $needle);
+}
+if (isset($_POST['make'])) {
+    $reservations = array();
+    foreach ($_POST as $field => $value) {
+        if (startsWith($field, 'select-room-') && $value == 'on') {
+            $id = $_POST[$field . '-id'];
+            $reservations[$id] = isset($_POST['select-extra-bed-' . $id]);
+        }
+    }
+    if (!isset($_POST['card_no'])) {
+        print 'No card selected';
+        exit;
+    }
+    $start_date = $_SESSION['start_date'];
+    $end_date = $_SESSION['end_date'];
+    $username = $_SESSION['username'];
+    $card_no = $_POST['card_no'];
+    $days = $_SESSION['days'];
+    $total_cost = 0;
+    $location = $_SESSION['location'];
+    foreach ($reservations as $num => $extra) {
+        $total_cost += $_SESSION['room-' . $num . '-cost'] * $days;
+        if ($extra) {
+            $total_cost += $_SESSION['room-' . $num . '-cost-extra-bed'] * $days;
+        }
+    }
+    $sql = "INSERT INTO reservation VALUES (DEFAULT, :start_date, :end_date, false, :total_cost, :card_no, :username)";
+    $st = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $st->execute(array(':start_date' => date("Y-m-d H:i:s", $start_date), ':end_date' => date("Y-m-d H:i:s", $end_date),
+        ':total_cost' => $total_cost, ':card_no' => $card_no, ':username' => $username));
+    $reservation_id = $db->lastInsertId();
+    foreach ($reservations as $room_no => $extra_bed) {
+        $sql = "INSERT INTO reservation_has_room VALUES (:reservation_id, :location, :room_no, :extra_bed)";
+        $st = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $st->execute(array(':reservation_id' => $reservation_id, ':location' => $location, ':room_no' => $room_no, ':extra_bed' => $extra_bed));
+    }
+    header('Location: confirmation-screen.php');
+    exit;
+}
 require 'start.php';
 ?>
 
@@ -36,7 +78,7 @@ require 'start.php';
     </div>
 
     <div id="selectrooms">
-        <form>
+        <form method="post">
             <table class="striped">
                 <thead>
                 <th data-field='roomnum'>Room Number</th>
@@ -53,8 +95,11 @@ require 'start.php';
                 <?php
                 if (isset($_POST["location"])) {
                     $location = $_POST["location"];
+                    $_SESSION['location'] = $location;
                     $start_date = strtotime($_POST["start_date"]);
                     $end_date = strtotime($_POST["end_date"]);
+                    $_SESSION['start_date'] = $start_date;
+                    $_SESSION['end_date'] = $end_date;
                     if (!$start_date || !$end_date
                         || $start_date - time() < 0 || $end_date - $start_date < 0) {
                         print "Bad date";
@@ -69,14 +114,21 @@ WHERE (DATE(start_date) >= :start_date AND DATE(end_date) <= :start_date OR (DAT
                         $st->execute(array(':location' => $location, ':start_date' => date("Y-m-d H:i:s",
                             $start_date), ':end_date' => date("Y-m-d H:i:s", $end_date)));
                         $rows = $st->fetchAll();
+                        $days = floor(($end_date - $start_date) / (60*60*24));
+                        $_SESSION['days'] = $days;
                         foreach ($rows as $row) {
                             $num = $row['num'];
                             $category = $row['category'];
                             $people = $row['people'];
                             $cost = $row['cost'];
                             $cost_extra_bed = $row['cost_extra_bed'];
-                            $js1 = "if(this.checked)$(\"#total_cost\").text(parseInt($(\"#total_cost\").text())+{$cost}); else $(\"#total_cost\").text(parseInt($(\"#total_cost\").text())-{$cost});";
-                            $js2 = "if(this.checked)$(\"#total_cost\").text(parseInt($(\"#total_cost\").text())+{$cost_extra_bed}); else $(\"#total_cost\").text(parseInt($(\"#total_cost\").text())-{$cost_extra_bed});";
+                            $_SESSION['room-' . $num . '-cost'] = $cost;
+                            $_SESSION['room-' . $num . '-cost-extra-bed'] = $cost_extra_bed;
+                            $js1cost = $cost * $days;
+                            $js2cost = $cost_extra_bed * $days;
+                            // Calculate number of days and then figure out cost for js
+                            $js1 = "if(this.checked)$(\"#total_cost\").text(parseInt($(\"#total_cost\").text())+{$js1cost}); else $(\"#total_cost\").text(parseInt($(\"#total_cost\").text())-{$js1cost});";
+                            $js2 = "if(this.checked)$(\"#total_cost\").text(parseInt($(\"#total_cost\").text())+{$js2cost}); else $(\"#total_cost\").text(parseInt($(\"#total_cost\").text())-{$js2cost});";
                             print "<tr>
     <td>{$num}</td>          <!-- variable num -->
     <td>{$category}</td>     <!-- variable category -->
@@ -84,11 +136,12 @@ WHERE (DATE(start_date) >= :start_date AND DATE(end_date) <= :start_date OR (DAT
     <td>{$cost}</td>          <!-- variable cost -->
     <td>{$cost_extra_bed}</td>           <!-- variable cost_extra_bed -->
     <td>
+        <input type='hidden' name='select-room-{$num}-id' value='{$num}'>
         <input type='checkbox' class='filled-in' id='select-room-{$num}' name='select-room-{$num}' onchange='{$js1}'/>
         <label for='select-room-{$num}'></label>
     </td>
     <td>
-        <input type='checkbox' class='filled-in' id='select-extra-bed-{$num}' name='select-room-{$num}' onchange='{$js2}'/>
+        <input type='checkbox' class='filled-in' id='select-extra-bed-{$num}' name='select-extra-bed-{$num}' onchange='{$js2}'/>
         <label for='select-extra-bed-{$num}'></label>
     </td>
 </tr>";
@@ -105,7 +158,7 @@ WHERE (DATE(start_date) >= :start_date AND DATE(end_date) <= :start_date OR (DAT
             <div class="row">
                 <div class="input-field col s6">
                     <!-- fill with payement options -->
-                    <select class="browser-default">
+                    <select name="card_no" class="browser-default">
                         <option value="" disabled selected>Select a card</option>
                         <?php
                         $sql = "SELECT card_no % 10000 as last, card_no FROM payment WHERE username = :username";
@@ -124,6 +177,7 @@ WHERE (DATE(start_date) >= :start_date AND DATE(end_date) <= :start_date OR (DAT
                     <a href="payment-info.php" class="waves-effect waves-light btn">Manage Cards</a>
                 </div>
             </div>
+            <input type="hidden" name="make" value="true">
             <button type="submit" class="waves-effect waves-light btn">Submit</button>
         </form>
     </div>
