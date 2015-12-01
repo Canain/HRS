daf835ab38807a5ea8d1c2c0c0c45e45c71c1ecd
nikhil-kulkarni <nikhil896@gmail.com>
2015-12-01 00:28:26 -0500

    View Revenue Report
    Not confirmed working

--- view-revenue-report.php
+++ view-revenue-report.php
@@ -1,23 +1,33 @@
 <?php
 require 'base.php';
 require 'start.php';
+try {
+    $sql = 'SELECT MONTHNAME(start_date) as month, location, sum(total_cost) as total_revenue FROM reservation NATURAL JOIN reservation_has_room';
+    $st = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $st->execute();
+    $total_revenue = $st->fetch()['total_revenue'];
+    $location = $st->fetch()['location'];
+    $month = $st->fetch()['month'];
+} catch (PDOException $ex) {
+    print $ex;
+}
 ?>
 <div id="selectrooms">
     <form>
         <table class="striped">
             <thead>
-            <th data-field='month'>Month</th>
-            <th data-field='location'>Location</th>
-            <th data-field='totalrevenue'>Total Revenue</th>
+            <th>Month</th>
+            <th>Location</th>
+            <th>Total Revenue</th>
             <thead>
 
             <tbody>
             <!--Example row with data from room table-->
-            <tr>
-                <td>August</td>       <!-- variable month -->
-                <td>Orlando</td>      <!-- variable location -->
-                <td>21000</td>    <!-- variable totalrevenue -->
-            </tr>
+                <tr>
+                    <td name="month"><?php print $month ?></td>                                    <!-- variable month -->
+                    <td name="location"><?php print $location ?></td>                                <!-- variable location -->
+                    <td name="totalrevenue"><?php print $total_revenue ?></td>      <!-- variable totalrevenue -->
+                </tr>
             </tbody>
         </table>
     </form>
